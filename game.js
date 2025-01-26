e60f13977a26067d4014d81f06050b6d8c72d7e9
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2025-01-26 12:40:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,36 @@
+function radians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+class Cone {
+    constructor(resolution, height, radius) {
+        this.resolution = resolution;
+        this.height = height;
+        this.radius = radius;
+    }
+
+    vertices() {
+        const vertices = [[0, this.height, 0], [0, 0, 0]];
+        const indices = [];
+
+        for (let i = 0; i < this.resolution; i++) {
+            vertices.push([Math.sin(radians((360 / this.resolution) * i - 360 / this.resolution)) * this.radius, 0, Math.cos(radians((360 / this.resolution) * i - 360 / this.resolution)) * this.radius]);
+        }
+
+        for (let i = 0; i < this.resolution; i++) {
+            if (i != this.resolution - 1) {
+                indices.push(0, 2 + i, 3 + i, 1, 2 + i, 3 + i);
+                
+                continue;
+            }
+
+            indices.push(0, 2 + i, 2, 1, 2 + i, 2);
+        }
+
+        return [vertices, indices];
+    }
+};
+
 let cax = 0.0;
 let cay = 0.0;
 let caz = 0.0;
@@ -98,15 +131,9 @@ gl.attachShader(prog, fs);
 gl.linkProgram(prog);
 gl.validateProgram(prog);
 
-const vertices = [
-    0.0, 0.5, 0.0, 1.0, 0.0, 0.0,
-    -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-    0.5, -0.5, 0.0, 0.0, 0.0, 1.0
-];
+const vertices = [];
 
-const indices = [
-    0, 1, 2
-];
+const indices = [];
 
 const vbuf = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vbuf);
