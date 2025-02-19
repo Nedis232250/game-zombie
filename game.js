9adbb890d8e4ca73814d564e62fc22a262ca402e
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2025-02-18 22:57:00 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,36 +1,3 @@
-function radians(degrees) {
-    return degrees * (Math.PI / 180);
-}
-
-class Cone {
-    constructor(resolution, height, radius) {
-        this.resolution = resolution;
-        this.height = height;
-        this.radius = radius;
-    }
-
-    vertices() {
-        const vertices = [[0, this.height, 0], [0, 0, 0]];
-        const indices = [];
-
-        for (let i = 0; i < this.resolution; i++) {
-            vertices.push([Math.sin(radians((360 / this.resolution) * i - 360 / this.resolution)) * this.radius, 0, Math.cos(radians((360 / this.resolution) * i - 360 / this.resolution)) * this.radius]);
-        }
-
-        for (let i = 0; i < this.resolution; i++) {
-            if (i != this.resolution - 1) {
-                indices.push(0, 2 + i, 3 + i, 1, 2 + i, 3 + i);
-                
-                continue;
-            }
-
-            indices.push(0, 2 + i, 2, 1, 2 + i, 2);
-        }
-
-        return [vertices, indices];
-    }
-};
-
 let cax = 0.0;
 let cay = 0.0;
 let caz = 0.0;
